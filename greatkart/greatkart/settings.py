62d835018546297b89b81e1e837296b51c0fc556
abdur-tech <1622abdurrahmaan@gmail.com>
2024-05-11 14:59:39 +0530

    Account page

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -135,3 +135,18 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.INFO: "danger",
+}
+
+# SMTP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tempnoor1622@gmail.com'
+EMAIL_HOST_PASSWORD = 'ivgb yvju ihkv fadh'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

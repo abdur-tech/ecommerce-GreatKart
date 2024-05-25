14f253f68c94bf78d9c8ed2486322a2468f47788
abdur-tech <1622abdurrahmaan@gmail.com>
2024-05-25 21:24:24 +0530

    allowed host modified

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.31.33.57','django-greatkart-env.eba-9a3nj4rt.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -147,8 +147,8 @@ MESSAGE_TAGS = {
 # SMTP CONFIGURATION
 
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
